d56744c9d4bb350d530e99d25d9c7a0de6b68b9d
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2018-07-27 21:18:54 -0300

    instalando phinx e definindo configuracoes do BD

---
+++ configuracoes/banco_de_dados.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'desenvolvimento' => [
+        'driver'    => 'mysql',
+        'host'      => '127.0.0.1',
+        'database'  => 'financias',
+        'username'  => 'developer',
+        'password'  => 'password',
+        'charset'   => 'utf8', // define o charset do banco
+        'collation' => 'utf8_unicode_ci', // define o encode das tabelas
+    ]
+];
