95d175d666c51e8e6df99f049b8b5eff6d912994
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2018-08-04 21:46:54 -0300

    criando classe application

---
+++ src/Application.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Financias;
+
+class Application
+{
+    private $serviceContainer;
+
+    public function __construct(ServiceContainerInterface $serviceContainer)
+    {
+        $this->serviceContainer = $serviceContainer;
+    }
+
+    public function service(string $name)
+    {
+        return $this->serviceContainer->get($name);
+    }
+
+    public function addService(string $name, $service)
+    {
+        if (is_callable($service)) {
+            $this->serviceContainer->addLazy($name, $service);
+        } else {
+            $this->serviceContainer->add($name, $service);
+        }
+    }
+}
